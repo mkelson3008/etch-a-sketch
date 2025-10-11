0b254a327b11a7168e48c217fdb935e81b2cae1b
Michael <192384970+mkelson3008@users.noreply.github.com>
2025-10-11 17:28:56 -0400

    Finish step 2 of the project
    Added the hovering effect portion of the project.

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-let mainContainer = document.querySelector(".mainContainer")
+// Set up the grid
 for (let i = 0; i < 32; i++) {
+    let mainContainer = document.querySelector(".mainContainer")
     const div = document.createElement("div");
-    if (i % 2 === 0) {
-        div.classList.toggle("boxTypeOne");
-    }
-    else {
-        div.classList.toggle("boxTypeTwo");
-    }
+    div.classList.toggle("box");
     mainContainer.appendChild(div);
 }
+
+// Create the hovering effect expected when interacting with grid
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+        box.classList.add("hoveringColor");
+    });
+});
