7adb00589830039ce19814b5dc5bb4dd36bc5146
Michael <192384970+mkelson3008@users.noreply.github.com>
2025-10-11 19:21:14 -0400

    Complete step 3 of the project
    In addition to completing part 3 of the project, changes included refactoring existing code via
    making them modular.

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
 // Set up the grid
-for (let i = 0; i < 32; i++) {
-    let mainContainer = document.querySelector(".mainContainer")
-    const div = document.createElement("div");
-    div.classList.toggle("box");
-    mainContainer.appendChild(div);
+
+function createGrid(gridSize = 32) {
+    for (let i = 0; i < gridSize; i++) {
+        let mainContainer = document.querySelector(".mainContainer")
+        const div = document.createElement("div");
+        div.classList.toggle("box");
+        mainContainer.appendChild(div);
+    }
+
+    addHover();
 }
 
-// Create the hovering effect expected when interacting with grid
-const boxes = document.querySelectorAll(".box");
 
-boxes.forEach((box) => {
-    box.addEventListener("mouseover", () => {
-        box.classList.add("hoveringColor");
+// Create the hovering effect expected when interacting with grid
+function addHover() {
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", () => {
+            box.classList.add("hoveringColor");
+        });
     });
-});
+
+}
+
+function removeGrid() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.remove();
+    });    
+}
+
+createGrid();
+const gridSizeBtn = document.querySelector(".gridSizeBtn");
+const maxSize = 100;
+gridSizeBtn.addEventListener("click", () => {
+    const size = Number(prompt("Enter a new size", "64"));
+
+    if (size > maxSize){
+        alert("Size cannot exceed 100.");
+    }
+    else {
+        removeGrid();
+        createGrid(size);
+    }
+})
