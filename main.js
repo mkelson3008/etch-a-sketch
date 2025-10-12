0326c25a6d8b60df17ec9ddda25e2b2fd243907a
Michael <192384970+mkelson3008@users.noreply.github.com>
2025-10-11 22:05:12 -0400

    Complete the extra credit portion of the project

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // Set up the grid
-
+let opacityValue;
 function createGrid(gridSize = 32) {
     for (let i = 0; i < gridSize; i++) {
         let mainContainer = document.querySelector(".mainContainer")
         const div = document.createElement("div");
         div.classList.toggle("box");
+        div.dataset.opacity = .0;
         mainContainer.appendChild(div);
     }
 
@@ -18,7 +19,9 @@ function addHover() {
 
     boxes.forEach((box) => {
         box.addEventListener("mouseover", () => {
-            box.classList.add("hoveringColor");
+            colorValues = generateColor(box.dataset.opacity);
+            box.style.backgroundColor = colorValues[1];
+            box.dataset.opacity = colorValues[0];
         });
     });
 
@@ -31,7 +34,23 @@ function removeGrid() {
     });    
 }
 
-createGrid();
+function getRandomInt(max = 255) {
+    return Math.floor(Math.random() * max);
+}
+
+function generateColor(currentOpacity) {
+    let r = getRandomInt();
+    let g = getRandomInt();
+    let b = getRandomInt();
+    let setOpacity = parseFloat(currentOpacity) + .1;
+    let color = `rgba(${r}, ${g}, ${b}, ${setOpacity})`;
+
+    let returnValues = [setOpacity, color];
+    return returnValues;
+}
+
+
+
 const gridSizeBtn = document.querySelector(".gridSizeBtn");
 const maxSize = 100;
 gridSizeBtn.addEventListener("click", () => {
